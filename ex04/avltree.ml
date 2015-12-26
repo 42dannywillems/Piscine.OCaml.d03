7ec17b03a1dfc890ce39d30ad6faa5734f10eec8
Danny Willems <contact@danny-willems.be>
2015-12-26 11:31:22 +0100

    ex04: not finished. Focus on other exercices giving new knowledge about OCaml

---
+++ ex04/avltree.ml
@@ -0,0 +1,9 @@
+(* Not finished because I want to focus on other exercices giving new knowledge
+ * about OCaml.
+*)
+
+type 'a btree = Nil | Node of 'a * 'a btree * 'a btree
+
+let insert_avl avl value () = avl
+
+let delete_avl avl value () = avl
